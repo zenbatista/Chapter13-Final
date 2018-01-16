822889619c36804b65ef7b221317339a4d7247d7
zenbatista <harald.batista@yahoo.com>
2018-01-15 23:20:39 -0800

    Chapter 14 Time: 28:17

--- Chapter14/EmojiArt.swift
+++ Chapter14/EmojiArt.swift
@@ -8,17 +8,24 @@
 
 import Foundation
 
-struct EmojiArt{
+struct EmojiArt: Codable
+
+{
     
     var url: URL
     var emojis = [EmojiInfo]()
     
-    struct EmojiInfo {
+    struct EmojiInfo: Codable {
         let x: Int
         let y: Int
         let text: String
         let size: Int
     }
+    
+    var json: Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
     
     init(url: URL, emojis: [EmojiInfo]) {
         self.url = url
